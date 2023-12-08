f0ec3747a696e8861bc895b18f063c7457b1e7aa
geo318 <geo.lomidze@gmail.com>
2023-12-08 15:54:39 +0400

    feat: create news db model

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,21 +1,12 @@
-import { sqliteTable, text, int, integer } from 'drizzle-orm/sqlite-core'
+import { sqliteTable, text, int } from 'drizzle-orm/sqlite-core'
 
-export const user = sqliteTable('user', {
-  id: int('id').primaryKey(),
-  fullName: text('full_name'),
-  phone: integer('phone'),
-})
-
-export const post = sqliteTable('post', {
+export const news = sqliteTable('news', {
   id: int('id').primaryKey(),
   title_eng: text('title_eng').notNull().unique(),
   title_geo: text('title_geo').notNull().unique(),
+  title_rus: text('title_rus').notNull().unique(),
   content_eng: text('content_eng').notNull(),
   content_geo: text('content_geo').notNull(),
-  type: text('type').notNull(),
+  content_rus: text('content_rus').notNull(),
   thumbnail: text('thumbnail').notNull(),
-  banner: text('banner'),
-  slug: text('slug').notNull().unique(),
-  order: integer('order').$defaultFn(() => 0),
-  link: text('link'),
 })
