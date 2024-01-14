69f54e95fdb41cbedd01dff0135ff3caf48e327c
geo318 <geo.lomidze@gmail.com>
2024-01-14 22:04:02 +0400

    style: modify slider

--- components/NewsSlider/NewsSlider.tsx
+++ components/NewsSlider/NewsSlider.tsx
@@ -24,13 +24,13 @@ export async function NewsSlider({
 export const NewsSkeleton = () => (
   <Section className='w-full max-w-full overflow-x-auto'>
     <H tag='h2' size='md' className='lg:mb-16 mb-4'>
-      <div className='animate-pulse w-1/3 h-14 bg-zinc-200  rounded-md' />
+      <div className='animate-pulse w-1/3 h-14 bg-zinc-200 rounded-md' />
     </H>
-    <div className='flex basis-1/4 gap-10 w-full h-72'>
+    <div className='grid lg:grid-cols-4 grid-cols-1 gap-10 w-full h-72'>
       <div className='animate-pulse w-full aspect-video bg-zinc-200 rounded-[2rem]' />
-      <div className='animate-pulse w-full aspect-video bg-zinc-200 rounded-[2rem]' />
-      <div className='animate-pulse w-full aspect-video bg-zinc-200 rounded-[2rem]' />
-      <div className='animate-pulse w-full aspect-video bg-zinc-200 rounded-[2rem]' />
+      <div className='animate-pulse w-full aspect-video bg-zinc-200 rounded-[2rem] lg:block hidden' />
+      <div className='animate-pulse w-full aspect-video bg-zinc-200 rounded-[2rem] lg:block hidden' />
+      <div className='animate-pulse w-full aspect-video bg-zinc-200 rounded-[2rem] lg:block hidden' />
     </div>
   </Section>
 )
