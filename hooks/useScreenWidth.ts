374397ab1804ae7f48ee30e1e927be296c6f2c45
geo318 <geo.lomidze@gmail.com>
2024-01-14 22:15:12 +0400

    refactor: slider render

--- hooks/useScreenWidth.ts
+++ hooks/useScreenWidth.ts
@@ -3,14 +3,14 @@ import { useEffect, useState } from 'react'
 export const useScreenWidth = (screen = 700) => {
   const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined)
   const [isLoading, setIsLoading] = useState(true)
+  const check = window?.innerWidth < screen
 
   useEffect(() => {
     setIsMobile(() => {
-      const check = window.innerWidth < screen
       setIsLoading(false)
       return check
     })
-  }, [setIsMobile, screen])
+  }, [setIsMobile, screen, check])
 
   return { isMobile, isLoading }
 }
